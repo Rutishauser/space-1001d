af099f44d9aa74d4a0385f2f47e07dea136760d8
Oliver Rutishauser <rutishauser@yandex.ru>
2017-07-04 19:44:20 -0400

    to add dependences

---
+++ data/Content.js
@@ -0,0 +1,7 @@
+
+module.exports = function()
+{
+    this.find = function(v) {
+        return "Uncle Sam wears a tall top hat and blue jacket and is pointing straight ahead at the viewer. During World War I, this portrait of Sam was used as a recruiting poster.";
+    }
+};
